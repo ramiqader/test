a445c2f550c990133836b33c2073a451af668f8a
Rami Abdelqader <ramiabdelqader@Ramis-MBP.lan>
2024-07-12 18:48:53 -0500

    commit

--- src/components/favourites.js
+++ src/components/favourites.js
@@ -8,13 +8,23 @@ function Favourites() {
     try {
       const response = await fetch('https://testback-ji4g.onrender.com/favourite', {mode:'cors'});
       const data = await response.json();
-      setData(data);
+      setData(data.message);
       console.log({ data })
     }
     catch (e) {
       console.log(e)
     }
-  }
+  };
+  const makeAPICall2 = async () => {
+  axios.get('http://localhost:3001/favourite')
+  .then(response => {
+    setData(response.data.message);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+};
+
   useEffect(() => {
    makeAPICall();
   }, []);
