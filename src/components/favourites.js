b9a6433d438d0286dae461fdec917813880af1f9
Rami Abdelqader <ramiabdelqader@Ramis-MBP.lan>
2024-07-12 17:25:45 -0500

    commit

--- src/components/favourites.js
+++ src/components/favourites.js
@@ -1,7 +1,25 @@
-const Favourites = () => {
-    return ( 
-        <h1>Favourites</h1>
-    )
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Favourites() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('/favourites')
+      .then(response => {
+        setData(response.data.message);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>React and Node.js Integration</h1>
+      <p>Message from the server: {data}</p>
+    </div>
+  );
 }
 
-export default Favourites;
+export default Favourites;  
